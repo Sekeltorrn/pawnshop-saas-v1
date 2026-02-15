b91342802014a88f8689ccb219c9da26145d5412
Sekelt <fayevanramos@gmail.com>
2026-02-15 14:51:49 +0000

    Organized folder structure and modularized header/footer

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="bg-deep-obsidian border-t border-eva-purple/50 py-12 px-4">
+        </footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html class="dark" lang="en">
+<head>
+    </head>
+<body class="bg-background-dark font-display text-white...">
+    <div class="fixed inset-0 scanline-overlay z-50 opacity-20 pointer-events-none"></div>
